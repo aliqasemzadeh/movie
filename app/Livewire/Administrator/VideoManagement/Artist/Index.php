ab8b35bf823e67b297977423320c690c9b11ede8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-31 05:29:10 +0330

    Add administrator layout to country, genre, and artist index components

--- app/Livewire/Administrator/VideoManagement/Artist/Index.php
+++ app/Livewire/Administrator/VideoManagement/Artist/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Administrator\VideoManagement\Artist;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('layouts.administrator')]
     public function render()
     {
         return view('livewire.administrator.video-management.artist.index');
--- app/Livewire/Administrator/VideoManagement/Genre/Index.php
+++ app/Livewire/Administrator/VideoManagement/Genre/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Administrator\VideoManagement\Genre;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('layouts.administrator')]
     public function render()
     {
         return view('livewire.administrator.video-management.genre.index');
