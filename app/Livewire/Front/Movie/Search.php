f94f05377f4201679b5d654b9e326be6ce0e004b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-09-01 10:08:32 +0330

    Add `Search` Livewire component and Blade view for movie search
    - Introduced `Search` Livewire component with corresponding Blade template.
    - Placeholder content added to the template for initial setup.

---
+++ app/Livewire/Front/Movie/Search.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Front\Movie;
+
+use Livewire\Component;
+
+class Search extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.movie.search');
+    }
+}
